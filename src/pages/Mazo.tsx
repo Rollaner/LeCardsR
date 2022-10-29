930ee2fad9568c3f880c075f41aa8c0950aa3d58
MaximilianoValencia <cielodespejado@protonmail.com>
2022-10-29 16:35:20 -0300

    agregar mazo a firestore

--- src/pages/Mazo.tsx
+++ src/pages/Mazo.tsx
@@ -1,18 +1,48 @@
 import { IonBackButton,IonList,IonInput, IonButton,IonButtons ,IonContent,IonHeader,IonPage,IonTitle,IonToolbar, useIonViewDidLeave, IonItem} from '@ionic/react';
 import { useState } from 'react';
 import '../../src/theme/Mazo.css';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebaseapp from '../firebase/firebaseconfig';
+import { arrayUnion, getFirestore, updateDoc } from 'firebase/firestore';
+import { doc, getDoc } from "firebase/firestore";
+
+
+
 const Mazo: React.FC = () => {
+    
     const [nombre, DefinirNombre] = useState("");
     useIonViewDidLeave(() =>{
         DefinirNombre("Placeholder")
     });
 
+    const auth = getAuth();
+    let uid:String;
+    
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+            uid = user.uid;
+            // console.log(uid);
+        }
+    });
+
+    const db = getFirestore(firebaseapp);    
+    const mazosRef = doc(db, "Datos", "Mazos");
+    
     const handleSubmit = (event:any) => {
         event.preventDefault();
-        console.log(nombre)
-        alert(`El nombre del mazo es: ${nombre}`)
+        // console.log(nombre);
+        // alert(`El nombre del mazo es: ${nombre}  el usuario es : ${uid}`);
+        updateDoc(mazosRef, {
+            Mazos : arrayUnion({
+                "nombre" : nombre,
+                "uuid" : uid,
+            })
+        });
+        alert(`mazo de nombre : ${nombre} agregado al usuario de id : ${uid}`);
         event.target.reset();
-      }
+    }
+
+
     return (
         <IonPage color="dark">
             <IonHeader>
